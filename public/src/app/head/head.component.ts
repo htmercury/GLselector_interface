10abf02ff6d1793eb723185b827bbf048d4007a9
Ka Wong <kawong2020@u.northwestern.edu>
2018-08-21 13:43:15 -0500

    webcam

--- public/src/app/head/head.component.ts
+++ public/src/app/head/head.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-head',
@@ -6,21 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./head.component.css']
 })
 export class HeadComponent implements OnInit {
+  play = false;
 
   constructor() { }
 
   ngOnInit() {
+    let self = this;
+    $('.cam-wrapper').toggle();
+
+    $('#demo').click(function () {
+      $('.cam-wrapper').slideToggle(800);
+    })
+
+    $(".ui.toggle.button").click(function () {
+      $(this).toggleClass('start');
+      $(".ui.toggle.button i").toggleClass('play stop');
+      if (self.play) {
+        video.srcObject = null;
+      }
+      else {
+        if (navigator.mediaDevices.getUserMedia) {
+          navigator.mediaDevices.getUserMedia({ video: true })
+            .then(function (stream) {
+              video.srcObject = stream;
+            })
+            .catch(function (err0r) {
+              console.log("Something went wrong!");
+            });
+        }
+      }
+      self.play = !self.play;
+    })
+
     var video = document.querySelector("#videoElement");
 
-    if (navigator.mediaDevices.getUserMedia) {
-    //   navigator.mediaDevices.getUserMedia({ video: true })
-    //     .then(function (stream) {
-    //       video.srcObject = stream;
-    //     })
-    //     .catch(function (err0r) {
-    //       console.log("Something went wrong!");
-    //     });
-    // }
+
   }
 
 }
