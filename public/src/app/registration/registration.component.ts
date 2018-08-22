38cfcb1be5be39a5a6ef8ab0934cc1003183be73
Zachary Verghese <zacharyverghese@Zacharys-MacBook-Pro.local>
2018-08-22 17:17:27 -0500

    message

--- public/src/app/registration/registration.component.ts
+++ public/src/app/registration/registration.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../http.service';
 import {ActivatedRoute} from '@angular/router';
+declare var $: any;
 
 @Component({
   selector: 'app-registration',
@@ -12,6 +13,73 @@ newUser:object = {fname:"", lname:"", email:"", password:""};
   constructor( private _httpService:HttpService , private _route: ActivatedRoute ) { }
 
   ngOnInit() {
+    $(document).ready(function() {
+
+      var ct = 0;
+    
+      $('.next1').on('click', function(e) {
+    
+        e.preventDefault();
+    
+        $('#account').animate('slow', function() {
+    
+          if (ct > 0) {
+            $('#account').removeClass('transition visible');
+            $('#account').addClass('transition hidden');
+    
+          }
+          $('#account').css('display', 'none');
+    
+          $('#accountS').addClass('disabled');
+          $('#socialP').removeClass('disabled');
+          $("#social").transition('fly right');
+          $('body').css('background-color', '#06000a');
+          $('#social button').removeClass('inverted violet');
+          $('#social button').addClass('inverted blue');
+          ct++;
+    
+        });
+    
+      });
+    
+      $('.prev1').on('click', function(e) {
+    
+        e.preventDefault();
+        $('#accountS').removeClass('disabled');
+        $('#socialP').addClass('disabled');
+    
+        $('#social').animate('slow', function() {
+    
+          $('body').css('background-color', '#300032');
+          $('#social').transition('hide');
+          $("#account").transition('fly right');
+    
+        });
+    
+      });
+    
+      $('.prev2').on('click', function(m) {
+    
+        m.preventDefault();
+        $('#details').addClass('disabled');
+        $('#socialP').removeClass('disabled');
+    
+        $('#personal').animate('slow', function() {
+    
+          $('body').css('background-color', '#06000a');
+          $('#personal').transition('hide');
+    
+          $('#social').transition('fly right');
+        });
+    
+      });
+    
+      $('.submit').on('click', function(p) {
+        p.preventDefault();
+        // $('#personal').stop();
+      });
+    
+    });
   }
 
   createUser(){
@@ -22,4 +90,7 @@ newUser:object = {fname:"", lname:"", email:"", password:""};
       })
     
   }
+
+  //jQuery time
+
 }
