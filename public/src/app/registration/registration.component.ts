737f48236049c1fd71ed8810fc4295dbdb6a8fc8
Zachary Verghese <zacharyverghese@Zacharys-MacBook-Pro.local>
2018-08-23 10:46:19 -0500

    registration camera

--- public/src/app/registration/registration.component.ts
+++ public/src/app/registration/registration.component.ts
@@ -9,11 +9,68 @@ declare var $: any;
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
-newUser:object = {fname:"", lname:"", email:"", password:""};
+  play = false;
+  src_img = "data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==";
+  newUser:object = {fname:"", lname:"", email:"", password:""};
   constructor( private _httpService:HttpService , private _route: ActivatedRoute ) { }
 
   ngOnInit() {
-    $(document).ready(function() {
+    let self = this;
+      // CAMERA JQUERY 
+      console.log('self', self);
+      $("#cam").click(function () {
+        // use MediaDevices API
+        // docs: https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+        if (navigator.mediaDevices) {
+          console.log(navigator.mediaDevices)
+          // access the web cam
+          navigator.mediaDevices.getUserMedia({ video: true })
+            // permission granted:
+            .then(function (stream) {
+              $(this).toggleClass('start');
+              $("#cam i").toggleClass('play stop');
+              video.srcObject = stream;
+              let take_picture = document.getElementById('take-picture');
+              take_picture.addEventListener('click', takeSnapshot);
+              if (self.play) {
+                stream.getTracks().forEach(track => track.stop());
+                video.srcObject = null;
+              }
+              self.play = !self.play;
+            })
+            // permission denied:
+            .catch(function (error) {
+              document.body.textContent = 'Could not access the camera. Error: ' + error.name;
+            });
+        }
+  
+        function takeSnapshot() {
+          let img: any;
+          img = document.getElementById('capture');
+          var context;
+          var width = video.offsetWidth
+            , height = video.offsetHeight;
+  
+          canvas = canvas || document.createElement('canvas');
+          canvas.width = width;
+          canvas.height = height;
+  
+          context = canvas.getContext('2d');
+          context.drawImage(video, 0, 0, width, height);
+  
+          self.src_img = canvas.toDataURL('image/png');
+          // console.log(self.src_img);
+          var str= self.src_img;
+          var newstr = str.substring(22);
+          console.log(newstr);
+        }
+      })
+      
+      let video: any;
+      video = document.querySelector("#videoElement");
+      let canvas: any;
+  
+      // END CAMERA JQUERY 
 
       var ct = 0;
     
@@ -78,8 +135,7 @@ newUser:object = {fname:"", lname:"", email:"", password:""};
         p.preventDefault();
         $('#personal').stop();
       });
-    
-    });
+    ;
     
   }
 
